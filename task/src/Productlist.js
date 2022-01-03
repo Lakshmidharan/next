1889121acccaa37985bda73fd37c9e11c95e4860
Lakshmidharan <lakshmidharan2401@gmail.com>
2022-01-02 19:47:23 -0800

    c

--- task/src/Productlist.js
+++ task/src/Productlist.js
@@ -12,7 +12,7 @@ import { Link } from "react-router-dom";
             <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                 <thead>
                     <tr>
-                        <th>Name</th>
+                        <th>Name</th> 
                         <th>Position</th>
                         <th>Office</th>
                         <th>Age</th>
